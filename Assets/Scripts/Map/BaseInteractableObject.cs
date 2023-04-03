97e9820607bdbe8c3a6c7a628a66e8adf71f6806
Fabrizio Piermarocchi <fabrizio.pier.dv@gmail.com>
2023-04-02 22:52:18 -0300

    Update BaseInteractableObject.cs

--- Assets/Scripts/Map/BaseInteractableObject.cs
+++ Assets/Scripts/Map/BaseInteractableObject.cs
@@ -5,15 +5,12 @@ using System;
 public abstract class BaseInteractableObject : MonoBehaviour
 {
     public event Action OnPlayerLeaveTrigger;
-    public event Action OnPlayerEnterTrigger;
 
-   
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Blind"))
         {
             collision.GetComponent<PlayerInput>().OnInteractUE.AddListener(delegate {OnPlayerInteraction(collision.gameObject);});
-            OnPlayerEnterTrigger?.Invoke();
         }
     }
 
