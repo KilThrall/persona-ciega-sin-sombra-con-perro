de19be7f2bc7875410f4743bae67df5d2a473317
Fabrizio Piermarocchi <fabrizio.pier.dv@gmail.com>
2023-04-21 23:40:58 -0300

    orden general y candelabro

--- Assets/Scripts/FX/EchoSpriteLight.cs
+++ Assets/Scripts/FX/EchoSpriteLight.cs
@@ -11,11 +11,11 @@ public class EchoSpriteLight : MonoBehaviour
     private float lightFallOffDuration = 5;
     [SerializeField]
     private Vector2 targetScale;
-
-
+    [SerializeField]
+    private float scalingSpeed = 1;//EL CANDELABRO TIENE UNA LUZ MUY GRANDE Y SE TARDA MUCHO EN ESCALAR A ESO
     private void FixedUpdate()
     {
-        transform.localScale += (Vector3)((Time.deltaTime / lightFallOffDuration) * targetScale) + Vector3.forward;
+        transform.localScale += (Vector3)((Time.deltaTime*scalingSpeed / lightFallOffDuration) * targetScale) + Vector3.forward;
 
 
         LightDuration -= Time.deltaTime;
