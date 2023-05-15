3e9f1197dc7ddf8020938e5a26e300e11b30c008
Fabrizio Piermarocchi <fabrizio.pier.dv@gmail.com>
2023-05-14 23:01:15 -0300

    prints :skull:

--- Assets/Scripts/Player/ClimbPlayerMovement.cs
+++ Assets/Scripts/Player/ClimbPlayerMovement.cs
@@ -80,9 +80,6 @@ public class ClimbPlayerMovement : MonoBehaviour
             {
                 ledgeDetected = true;
                 ledgePosBot = wallCheck.position;
-
-                print("ledgeDetected");
-
             }
             else
             {
