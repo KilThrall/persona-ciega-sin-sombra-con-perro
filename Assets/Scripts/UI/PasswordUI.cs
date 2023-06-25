d254cb07cc4cc65ab1fc544ad61489d73db63482
kilthrall <nico.florido@hotmail.com>
2023-06-25 18:45:23 -0300

    Added OnPasswordFail

--- Assets/Scripts/UI/PasswordUI.cs
+++ Assets/Scripts/UI/PasswordUI.cs
@@ -1,27 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PasswordUI : MonoBehaviour
 {
     public const string PASSWORD_INTERACT_KEY = "PasswordInteracted";
     public const string PASSWORD_INPUT_KEY = "PasswordInput";
+    public const string PASSWORD_FAIL_KEY = "PasswordFail";
+
+    [SerializeField]
+    private UnityEvent onPasswordFail;
 
 
     private void Awake()
     {
         ActionsManager.SubscribeToAction(PASSWORD_INTERACT_KEY, OnInteract);
+        ActionsManager.SubscribeToAction(PASSWORD_FAIL_KEY, OnPasswordFail);
         gameObject.SetActive(false);
     }
 
     private void OnDestroy()
     {
         ActionsManager.UnsubscribeToAction(PASSWORD_INTERACT_KEY, OnInteract);
+        ActionsManager.UnsubscribeToAction(PASSWORD_FAIL_KEY, OnPasswordFail);
     }
 
     public void PlaySound(AudioClip clip)
     {
         GameUIManager.Instance.PlaySoundOneShot(clip);
+    }
+
+    private void OnPasswordFail(object obj)
+    {
+        onPasswordFail?.Invoke();
     }
 
     private void OnInteract(object obj)
