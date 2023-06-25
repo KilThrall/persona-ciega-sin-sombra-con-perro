d2a2cd5e07fb193772d3f6f490f497b60f0c7eb4
Fabrizio Piermarocchi <fabrizio.pier.dv@gmail.com>
2023-06-24 21:04:03 -0300

    OnUnPlug Added, tested on scene

--- Assets/Scripts/Props/Wire/ItemSocket.cs
+++ Assets/Scripts/Props/Wire/ItemSocket.cs
@@ -6,6 +6,7 @@ public class ItemSocket : MonoBehaviour
 {
     public bool ItemConnected => itemConnected;
     public UnityEvent onConnectionEvent;
+    public UnityEvent onDisconectEvent;
 
     [SerializeField][Tooltip("Donde se va a quedar el item/cable cuando quede enchufado, si es nulo va al centro del sprite")]
     private Transform connectionPosition;
@@ -22,10 +23,17 @@ public class ItemSocket : MonoBehaviour
             }
             return transform.position;
         }
-    } 
+    }
+    
     public void Connect()
     {
         onConnectionEvent.Invoke();
         itemConnected = true;
+    }
+
+    public void Disconect()
+    {
+        onDisconectEvent.Invoke();
+        itemConnected = false;
     }
 }
